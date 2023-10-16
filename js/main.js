518c947282bda11f029cd9d99345a2b63a7c8f34
Jomkit <j.jujaroen96@gmail.com>
2023-10-16 10:24:36 +0700

    Fixed a bug where upon logging in manually (and presumably, signing up, although I haven't tested that yet) the login/signup form was still displayed. Also added a 'submit' button for submitting stories but have not connected to the addStory function yet

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const $allStoriesList = $("#all-stories-list");
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
 
+const $mainNavLinks = $(".main-nav-links");
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
@@ -20,6 +21,7 @@ const $navLogOut = $("#nav-logout");
  */
 
 function hidePageComponents() {
+  console.debug("hidePageComponents");
   const components = [
     $allStoriesList,
     $loginForm,
